2c79d8974c77a28ba7871d25613f5976157680e6
Cayden <cayden@bio-matrixinc.com>
2025-11-17 09:28:28 -0500

    Replaced import with CDN

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
-
 const SUPABASE_URL = import.meta.env.NEXT_PUBLIC_SUPABASE_URL; 
 const SUPABASE_ANON_KEY = import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY; 
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 
 document.addEventListener('DOMContentLoaded', () => {
